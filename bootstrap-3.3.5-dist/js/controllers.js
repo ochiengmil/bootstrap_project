d43e72894616fde0cc10720c44511a191e03c591
ochiengmil <ochiengmil@gmail.com>
2015-12-01 15:42:56 +0300

    images and more

---
+++ bootstrap-3.3.5-dist/js/controllers.js
@@ -0,0 +1,28 @@
+//module represent angular app
+
+var app = angular.module("MyApp", []);
+app.controller('detailsController', function($scope)
+	{
+//$scope accces DOM - access variables
+		$scope.fname = "George";
+		$scope.lname = "Milanya";
+		$scope.course = "Mean Stack Course";
+		$scope.school = "The Dev School";
+		$scope.grade = "A";
+	});
+
+//2nd controller
+app.controller('carsController', function($scope)
+	{
+		$scope.cars = [ //here we create an array of objects
+
+			{type:"saloon",model:"mac2",brand:"Toyota",year:2006},
+			 {type:"station_wagon",model:"probox",brand:"Toyota",year:2009},
+			  {type:"prado",model:"pajero",brand:"Toyota",year:2010},
+			   {type:"pickup",model:"dmax",brand:"Isuzu",year:2005},
+			    {type:"saloon",model:"c600",brand:"mercedes",year:2011}
+
+		
+		];
+	
+	});
--- bootstrap-3.3.5-dist/js/jqueryuiwork.js
+++ bootstrap-3.3.5-dist/js/jqueryuiwork.js
@@ -1,9 +1,32 @@
 $(document).ready(function()
 {
+	
 
 
 	//here we create an accordion ..
 	$("#accordion").accordion();
+
 	$( "#tabs" ).tabs();
+	
+
+
+	// var listTemplate = '';
+	// listTemplate += '<ul>';
+	// listTemplate += '<li>';
+	// listTemplate += '<h1>'<a href="#repo_link">Repository title</a></h1>
+	// listTemplate += '<p>' Description</p>
+	// listTemplate += '<span>'Last updated: <time>12.00pm</time></span>
+	// listTemplate += '</li>'
+	// listTemplate +=	'</ul>'
+
+
+	
+ 					
+
+
+
+
+$('#open-source-container').html(listTemplate);
+
 
 });
