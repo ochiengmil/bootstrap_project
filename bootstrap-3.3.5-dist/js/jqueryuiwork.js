e973f50098f35ae9f92068f23a00bc698231024a
ochiengmil <ochiengmil@gmail.com>
2015-12-02 11:21:23 +0300

    add ajax and apis

---
+++ bootstrap-3.3.5-dist/js/jqueryuiwork.js
@@ -0,0 +1,51 @@
+$(document).ready(function()
+{
+	
+
+
+	//here we create an accordion ..
+	$("#accordion").accordion();
+
+	$( "#tabs" ).tabs();
+	
+
+
+	// var listTemplate = '';
+	// listTemplate += '<ul>';
+	// listTemplate += '<li>';
+	// listTemplate += '<h1>'<a href="#repo_link">Repository title</a></h1>
+	// listTemplate += '<p>' Description</p>
+	// listTemplate += '<span>'Last updated: <time>12.00pm</time></span>
+	// listTemplate += '</li>'
+	// listTemplate += '</ul>'
+
+
+	
+
+
+	url = 'https://api.github.com/users/ochiengmil/repos';
+	$.get(url, function(data){
+
+			var listTemplate='';
+			listTemplate += '<ul>';
+
+		data.forEach(function(repository){
+			listTemplate += '<li>';
+			listTemplate += '<h1><a href="'+repository.html_url+'">'+repository.name+'</a></h1>';
+			listTemplate += '<p>Description</p>';
+			listTemplate +='<span>Last updated: <time>12:00pm</time></span>';
+			listTemplate += '</li>';
+		});
+
+			listTemplate += '<ul>';
+			$('#open-source-container').html(listTemplate);
+	  });
+
+
+
+
+
+	//$('#open-source-container').html(listTemplate);
+
+
+});
